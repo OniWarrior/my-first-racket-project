61490a6dfe1413f96b4a71639040bd1b838dedd9
Stephen Aranda <aranda.stephen88@gmail.com>
2023-02-28 20:20:09 -0600

    defined classify

--- myFirstRacket.rkt
+++ myFirstRacket.rkt
@@ -13,7 +13,7 @@
 |#
 
 
-#| Divisible-by-x? function: Take an integer and determine if
+#| divisible-by-x? function: Take an integer and determine if
    it can evenly divided by x
  |#
 
@@ -21,7 +21,7 @@
     (λ (x)
       ( if (zero? (modulo x 2))
         #t
-      "Not evenly divisible")))
+        #f)))
 
 #| Here's the function call with the example x that should evaluate to true|#
 (divisible-by-x? 10)
@@ -29,37 +29,53 @@
 #| Here's the function call with the example x that should evaluate to false  |#
 (divisible-by-x? 11)
 
+#|----------------------------------------------------------------------------|#
+
+'("---------------------------------------------------------------------------")
 
 #| function-9 function : Defines a function that takes a function as an argument.
   Then passes an int, which is x, to that function and adds 6 to it.
 |#
 
-(define function-9
-  (λ (x)
-    (+ x 6)))
-
+#|Example function that will be passed into function-9|#
+(define (add-nine-to-five)
+  (+ 9 5))
+#|Here's the actual function definition for function-9|#
+(define (function-9 func)
+  (func))
 
  #| Here's the function call as an example to show that this function works |#
-(function-9 9)
+(function-9 add-nine-to-five)
 
+#|--------------------------------------------------------------------------|#
+'("-------------------------------------------------------------------------")
 
-# | my-map function: define a function that accepts another function as an argument.
+#| my-map function: define a function that accepts another function as an argument.
     this function is suppose to mimic the behavior of map.
     addNum is a helper function that just performs addition to the arg sent to it.
 |#
+
+#|Example function to demonstrate the operation performed on elements in my-map
+before returned value is put into a new list.|#
 (define addNum
       (λ (x)
         (+ x x)))
 
-(define (my-map lst)
+
+#|Here's the actual definition of my-map|#
+(define (my-map func lst)
   (if (empty? lst)
       empty
-      (cons (addNum (first lst)) (my-map (rest lst)))))
+      (cons (func (first lst)) (my-map func (rest lst)))))
 
 
 #| Here's the function call to my-map and a list argument|#
-(my-map '(2 3 4 5 5))
+(my-map addNum '(2 3 4 5 5))
 
+(my-map addNum '(5 3 5 6))
+
+#|----------------------------------------------------------------------|#
+'("---------------------------------------------------------------------")
 
 #| pair-up function: define a function that takes two lists as arguments, then returns
    a single list of pairs.
@@ -77,7 +93,55 @@
 #| Here's the function call with two example lists as arguments.|#
 (pair-up '(1 2 3) '("yes" "bananas" "hello"))
 
+#|-----------------------------------------------------------------------------|#
+'("----------------------------------------------------------------------------")
 
+#|classify function: a function that accepts a boolean test that tests an atomic value
+      and/or a list of elements as arguments.
+|#
+
+
+#|Helper function that creates a list for the truth values within the list provided.
+  Truth is determined by testing with the parameter test argument sent to the function
+|#
+(define (truth-list test lst)
+  (if (empty? lst)
+      empty
+      (if (test (first lst))
+          (append (list (first lst)) (truth-list test (rest lst)))
+          (truth-list test (rest lst)))))
+
+#|Helper function that creates a list for the false values withing the list provided.
+  Falseness is determenined by testing with the parameter test argument sent to the function.
+|#
+(define (false-list test lst)
+  (if (empty? lst)
+      empty
+      (if (test (first lst))
+          (false-list test (rest lst))
+          (append (list (first lst)) (false-list test (rest lst))))))
+
+
+#|Here's the actual classify function. Accepts a parameter test and an argument.
+ Returns a list containing two lists. The first list is a list of truth values, the
+ other list contains the false values.
+ |#
+(define (classify test lst)
+  
+  (if (empty? lst)
+      (list '() '())
+      (list
+        (append (truth-list test lst))
+        (append (false-list test lst))
+       )))
+
+#|Here's the function call to classify to show how it works|#
+(classify positive? '(1 -2 3 -5 6))
+
+(classify string? '("hello" 4 5 "ye" 7.9))
+
+#|------------------------------------------------------------------------------|#
+'("----------------------------------------------------------------------------")
 
 
 
